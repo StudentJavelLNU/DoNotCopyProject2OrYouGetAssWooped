4f7d9fda2e82bbe4e776db79df63509750224a29
StudentJavelLNU.se <dc222dz@student.lnu.se>
2018-11-22 14:53:01 +0100

    Next implement print actions in philosopher  Changes to be committed: 	modified:   src/DiningPhilosopher.java 	modified:   src/Philosopher.java

--- src/DiningPhilosopher.java
+++ src/DiningPhilosopher.java
@@ -7,98 +7,110 @@ import java.util.concurrent.TimeUnit;
 
 public class DiningPhilosopher {
 
-	/*
-	 * Controls whether logs should be shown on the console or not.
-	 * Logs should print events such as: state of the philosopher, and state of the chopstick
-	 * 		for example: philosopher # is eating;
-	 * 		philosopher # picked up his left chopstick (chopstick #)
-	 */
-	public boolean DEBUG = false;
-	private final int NUMBER_OF_PHILOSOPHERS = 5;
-	private int SIMULATION_TIME = 10000;
-	private int SEED = 0;
+    /*
+     * Controls whether logs should be shown on the console or not.
+     * Logs should print events such as: state of the philosopher, and state of the chopstick
+     * 		for example: philosopher # is eating;
+     * 		philosopher # picked up his left chopstick (chopstick #)
+     */
+    public static volatile boolean DEBUG = false;   //Volatile since only accessed from constructionthread
+                                                    //No need for atomicity
+    private final int NUMBER_OF_PHILOSOPHERS = 5;
+    private int SIMULATION_TIME = 10000;
+    private int SEED = 0;
 
-	ExecutorService executorService = null;
-	ArrayList<Philosopher> philosophers = null;
-	ArrayList<ChopStick> chopSticks = null;
+    ExecutorService executorService = null;
+    ArrayList<Philosopher> philosophers = null;
+    ArrayList<ChopStick> chopSticks = null;
 
-	public void start() throws InterruptedException {
-		try {
-			/*
-			 * First we start two non-adjacent threads, which are T1 and T3
-			 */
-			for (int i = 1; i < NUMBER_OF_PHILOSOPHERS; i+=2) {
-				executorService.execute(philosophers.get(i));
-				Thread.sleep(50); //makes sure that this thread kicks in before the next one
-			}
+    public void start() throws InterruptedException {
+        try {
+            /*
+             * First we start two non-adjacent threads, which are T1 and T3
+             */
+            for (int i = 1; i < NUMBER_OF_PHILOSOPHERS; i += 2) {
+                executorService.execute(philosophers.get(i));
+                Thread.sleep(50); //makes sure that this thread kicks in before the next one
+            }
 
-			/*
-			 * Now we start the rest of the threads, which are T0, T2, and T4
-			 */
-			for (int i = 0; i < NUMBER_OF_PHILOSOPHERS; i+=2) {
-				executorService.execute(philosophers.get(i));
-				Thread.sleep(50); //makes sure that this thread kicks in before the next one
-			}
+            /*
+             * Now we start the rest of the threads, which are T0, T2, and T4
+             */
+            for (int i = 0; i < NUMBER_OF_PHILOSOPHERS; i += 2) {
+                executorService.execute(philosophers.get(i));
+                Thread.sleep(50); //makes sure that this thread kicks in before the next one
+            }
 
-			// Main thread sleeps till time of simulation
-			Thread.sleep(SIMULATION_TIME);
+            // Main thread sleeps till time of simulation
+            Thread.sleep(SIMULATION_TIME);
 
-			/*	TODO
-			 *  Stop all philosophers.
-			 *  Add comprehensive comments to explain your implementation.
-			 */
+            /*	TODO
+             *  Stop all philosophers.
+             *  Add comprehensive comments to explain your implementation.
+             */
 
 
-		} finally {
-			executorService.shutdown();
-			executorService.awaitTermination(10, TimeUnit.MILLISECONDS);
-		}
-	}
 
-	public void initialize(int simulationTime, int randomSeed) {
-		SIMULATION_TIME = simulationTime;
-		SEED = randomSeed;
+        } finally {
+            executorService.shutdown();
+            executorService.awaitTermination(10, TimeUnit.MILLISECONDS);
+        }
+    }
 
-		philosophers = new ArrayList<Philosopher>(NUMBER_OF_PHILOSOPHERS);
-		chopSticks = new ArrayList<ChopStick>(NUMBER_OF_PHILOSOPHERS);
+    public void initialize(int simulationTime, int randomSeed) {
+        SIMULATION_TIME = simulationTime;
+        SEED = randomSeed;
 
-		//create the executor service
-		executorService = Executors.newFixedThreadPool(NUMBER_OF_PHILOSOPHERS);
+        philosophers = new ArrayList<Philosopher>(NUMBER_OF_PHILOSOPHERS);
+        chopSticks = new ArrayList<ChopStick>(NUMBER_OF_PHILOSOPHERS);
 
-		/* TODO
-		 * Add chopsticks,
-		 * Add philosophers, and
-		 * Assign the corresponding chopsticks.
-		 * Add comprehensive comments to explain your implementation.
-		 */
+        //create the executor service
+        executorService = Executors.newFixedThreadPool(NUMBER_OF_PHILOSOPHERS);
 
-	}
+        //Adding chopsticks
+        for (int i = 0; i < NUMBER_OF_PHILOSOPHERS; i++) {
+            chopSticks.add(new ChopStick(i)); // Adding with id i
+        }
+        //Add philosophers and hand out chopsticks
+        for (int i = 0; i < NUMBER_OF_PHILOSOPHERS - 1; i++) {//offsetting from 0, left chopstick is phil_id+1
+            Philosopher phil;
+            //Give the last philosopher chopstick 4 and 0;
+            if (i == NUMBER_OF_PHILOSOPHERS) {
+                phil = new Philosopher(i, chopSticks.get(0), chopSticks.get(i), randomSeed);
+                philosophers.add(phil);
+            } else {
+                phil = new Philosopher(i, chopSticks.get(i + 1), chopSticks.get(i), randomSeed);
+                philosophers.add(phil);
 
-	public ArrayList<Philosopher> getPhilosophers() {
-		return philosophers;
-	}
+            }
+        }
+    }
 
-	/*
-	 * The following code prints a table where each row corresponds to one of the Philosophers,
-	 * Columns correspond to the Philosopher ID (PID), average thinking time (ATT), average eating time (AET), average hungry time (AHT), number of thinking turns(#TT), number of eating turns(#ET), and number of hungry turns(#HT).
-	 * This table should be printed regardless of the DEBUG value
-	 */
-	public void printTable() {
-		DecimalFormat df2 = new DecimalFormat(".##");
-		System.out.println("\n---------------------------------------------------");
-		System.out.println("PID \tATT \tAET \tAHT \t#TT \t#ET \t#HT");
+    public ArrayList<Philosopher> getPhilosophers() {
+        return philosophers;
+    }
 
-		for (Philosopher p : philosophers) {
-			System.out.println(p.getId() + "\t"
-					+ df2.format(p.getAverageThinkingTime()) + "\t"
-					+ df2.format(p.getAverageEatingTime()) + "\t"
-					+ df2.format(p.getAverageHungryTime()) + "\t"
-					+ p.getNumberOfThinkingTurns() + "\t"
-					+ p.getNumberOfEatingTurns() + "\t"
-					+ p.getNumberOfHungryTurns() + "\t");
-		}
+    /*
+     * The following code prints a table where each row corresponds to one of the Philosophers,
+     * Columns correspond to the Philosopher ID (PID), average thinking time (ATT), average eating time (AET), average hungry time (AHT), number of thinking turns(#TT), number of eating turns(#ET), and number of hungry turns(#HT).
+     * This table should be printed regardless of the DEBUG value
+     */
+    public void printTable() {
+        DecimalFormat df2 = new DecimalFormat(".##");
+        System.out.println("\n---------------------------------------------------");
+        System.out.println("PID \tATT \tAET \tAHT \t#TT \t#ET \t#HT");
 
-		System.out.println("---------------------------------------------------\n");
-	}
+        for (Philosopher p : philosophers) {
+            System.out.println(p.getId() + "\t"
+                    + df2.format(p.getAverageThinkingTime()) + "\t"
+                    + df2.format(p.getAverageEatingTime()) + "\t"
+                    + df2.format(p.getAverageHungryTime()) + "\t"
+                    + p.getNumberOfThinkingTurns() + "\t"
+                    + p.getNumberOfEatingTurns() + "\t"
+                    + p.getNumberOfHungryTurns() + "\t");
+        }
+
+        System.out.println("---------------------------------------------------\n");
+    }
 
 }
--- src/Philosopher.java
+++ src/Philosopher.java
@@ -1,108 +1,142 @@
 import java.util.Random;
 
 public class Philosopher implements Runnable {
-	
-	private int id;
-	
-	private final ChopStick leftChopStick;
-	private final ChopStick rightChopStick;
-	
-	private Random randomGenerator = new Random();
-	
-	private int numberOfEatingTurns = 0;
-	private int numberOfThinkingTurns = 0;
-	private int numberOfHungryTurns = 0;
+    private enum State {
+        EATING, THINKING, HUNGRY,PICK_LEFT,PICK_RIGHT,DROPPED_LEFT,DROPPED_RIGHT;
+    }
 
-	private double thinkingTime = 0;
-	private double eatingTime = 0;
-	private double hungryTime = 0;
-	
-	public Philosopher(int id, ChopStick leftChopStick, ChopStick rightChopStick, int seed) {
-		this.id = id;
-		this.leftChopStick = leftChopStick;
-		this.rightChopStick = rightChopStick;
-		
-		/*
-		 * set the seed for this philosopher. To differentiate the seed from the other philosophers, we add the philosopher id to the seed.
-		 * the seed makes sure that the random numbers are the same every time the application is executed
-		 * the random number is not the same between multiple calls within the same program execution 
-		 
-		 * NOTE
-		 * In order to get the same average values use the seed 100, and set the id of the philosopher starting from 0 to 4 (0,1,2,3,4). 
-		 * Each philosopher sets the seed to the random number generator as seed+id. 
-		 * The seed for each philosopher is as follows:
-		 * 	 	P0.seed = 100 + P0.id = 100 + 0 = 100
-		 * 		P1.seed = 100 + P1.id = 100 + 1 = 101
-		 * 		P2.seed = 100 + P2.id = 100 + 2 = 102
-		 * 		P3.seed = 100 + P3.id = 100 + 3 = 103
-		 * 		P4.seed = 100 + P4.id = 100 + 4 = 104
-		 * Therefore, if the ids of the philosophers are not 0,1,2,3,4 then different random numbers will be generated.
-		 */
-		
-		randomGenerator.setSeed(id+seed);
-	}
-	public int getId() {
-		return id;
-	}
+    private int id;
 
-	public double getAverageThinkingTime() {
-		/* TODO
-		 * Return the average thinking time
-		 * Add comprehensive comments to explain your implementation
-		 */
-		return 0;
-	}
+    private final ChopStick leftChopStick;
+    private final ChopStick rightChopStick;
 
-	public double getAverageEatingTime() {
-		/* TODO
-		 * Return the average eating time
-		 * Add comprehensive comments to explain your implementation
-		 */
-		return 0;
-	}
+    private Random randomGenerator = new Random();
 
-	public double getAverageHungryTime() {
-		/* TODO
-		 * Return the average hungry time
-		 * Add comprehensive comments to explain your implementation
-		 */
-		return 0;
-	}
-	
-	public int getNumberOfThinkingTurns() {
-		return numberOfThinkingTurns;
-	}
-	
-	public int getNumberOfEatingTurns() {
-		return numberOfEatingTurns;
-	}
-	
-	public int getNumberOfHungryTurns() {
-		return numberOfHungryTurns;
-	}
+    private int numberOfEatingTurns = 0;
+    private int numberOfThinkingTurns = 0;
+    private int numberOfHungryTurns = 0;
 
-	public double getTotalThinkingTime() {
-		return thinkingTime;
-	}
+    private double thinkingTime = 0;
+    private double eatingTime = 0;
+    private double hungryTime = 0;
+    private State state;
 
-	public double getTotalEatingTime() {
-		return eatingTime;
-	}
 
-	public double getTotalHungryTime() {
-		return hungryTime;
-	}
+    /**
+     * @param id
+     * @param leftChopStick
+     * @param rightChopStick
+     * @param seed
+     */
+    public Philosopher(int id, ChopStick leftChopStick, ChopStick rightChopStick, int seed) {
+        this.id = id;
+        this.leftChopStick = leftChopStick;
+        this.rightChopStick = rightChopStick;
 
-	@Override
-	public void run() {
-		/* TODO
-		 * Think,
-		 * Hungry,
-		 * Eat,
-		 * Repeat until thread is interrupted
-		 * Increment the thinking/eating turns after thinking/eating process has finished.
-		 * Add comprehensive comments to explain your implementation, including deadlock prevention/detection
-		 */
-		
-	}
+        /*
+         * set the seed for this philosopher. To differentiate the seed from the other philosophers, we add the philosopher id to the seed.
+         * the seed makes sure that the random numbers are the same every time the application is executed
+         * the random number is not the same between multiple calls within the same program execution
+
+         * NOTE
+         * In order to get the same average values use the seed 100, and set the id of the philosopher starting from 0 to 4 (0,1,2,3,4).
+         * Each philosopher sets the seed to the random number generator as seed+id.
+         * The seed for each philosopher is as follows:
+         * 	 	P0.seed = 100 + P0.id = 100 + 0 = 100
+         * 		P1.seed = 100 + P1.id = 100 + 1 = 101
+         * 		P2.seed = 100 + P2.id = 100 + 2 = 102
+         * 		P3.seed = 100 + P3.id = 100 + 3 = 103
+         * 		P4.seed = 100 + P4.id = 100 + 4 = 104
+         * Therefore, if the ids of the philosophers are not 0,1,2,3,4 then different random numbers will be generated.
+         */
+
+        randomGenerator.setSeed(id + seed);
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public double getAverageThinkingTime() {
+        /* TODO
+         * Return the average thinking time
+         * Add comprehensive comments to explain your implementation
+         */
+        return 0;
+    }
+
+    public double getAverageEatingTime() {
+        /* TODO
+         * Return the average eating time
+         * Add comprehensive comments to explain your implementation
+         */
+        return 0;
+    }
+
+    public double getAverageHungryTime() {
+        /* TODO
+         * Return the average hungry time
+         * Add comprehensive comments to explain your implementation
+         */
+        return 0;
+    }
+
+    public int getNumberOfThinkingTurns() {
+        return numberOfThinkingTurns;
+    }
+
+    public int getNumberOfEatingTurns() {
+        return numberOfEatingTurns;
+    }
+
+    public int getNumberOfHungryTurns() {
+        return numberOfHungryTurns;
+    }
+
+    public double getTotalThinkingTime() {
+        return thinkingTime;
+    }
+
+    public double getTotalEatingTime() {
+        return eatingTime;
+    }
+
+    public double getTotalHungryTime() {
+        return hungryTime;
+    }
+
+    @Override
+    public void run() {
+        state = State.THINKING;
+        /* TODO
+         * Hungry,
+         * Eat,
+         * Repeat until thread is interrupted
+         * Increment the thinking/eating turns after thinking/eating process has finished.
+         * Add comprehensive comments to explain your implementation, including deadlock prevention/detection
+         */
+
+
+    }
+
+    /**
+     * Prints state of philosopher(if DiningPhilosopher.DEBUG = true)
+     */
+    public void printState() {
+        if (DiningPhilosopher.DEBUG == true) {
+            switch (state) {
+                case EATING:
+                    System.out.println("Philosopher " + this.getId() + ": is Eating");
+                    break;
+                case THINKING:
+                    System.out.println("Philosopher " + this.getId() + ": is Thinking");
+                    break;
+                case HUNGRY:
+                    System.out.println("Philosopher " + this.getId() + ": is Hungry");
+                    break;
+
+            }
+        }
+    }
 }
